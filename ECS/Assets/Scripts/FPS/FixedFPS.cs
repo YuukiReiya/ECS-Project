67f8c1efcdad68476715841a94c9ed9a716e003b
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-05 12:34:37 +0900

    プロジェクトデータ追加

---
+++ ECS/Assets/Scripts/FPS/FixedFPS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedFPS : MonoBehaviour
+{
+    [SerializeField]
+    uint fixedFPS = 60;
+    public uint FPS { get { return fixedFPS; } }
+
+    private void Awake()
+    {
+        Application.targetFrameRate = (int)fixedFPS;
+    }
+}

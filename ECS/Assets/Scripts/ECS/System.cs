45642ee7d84d5ed820488c3ce44149f9a512b5ea
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-10 09:56:57 +0900

    update to unity version

--- ECS/Assets/Scripts/ECS/System.cs
+++ ECS/Assets/Scripts/ECS/System.cs
@@ -10,25 +10,26 @@ using Unity.Mathematics;
 namespace ECS
 {
 
-    public class System : JobComponentSystem
-    {
-        private struct JobTask : IJobProcessComponentData<Translation, Rotation, ECS.Entity>
-        {
-            public float DeltaTime;
-            public void Execute(ref Translation pos,ref Rotation rot,ref ECS.Entity entity )
-            {
-                pos.Value.y -= entity.val * DeltaTime;
-            }
-        }
+    //public class System : JobComponentSystem
+    //{
+    //    [Obsolete]
+    //    private struct JobTask : IJobProcessComponentData<Translation, Rotation, ECS.Entity>
+    //    {
+    //        public float DeltaTime;
+    //        public void Execute(ref Translation pos,ref Rotation rot,ref ECS.Entity entity )
+    //        {
+    //            pos.Value.y -= entity.val * DeltaTime;
+    //        }
+    //    }
 
-        protected override JobHandle OnUpdate(JobHandle inputDeps)
-        {
-            var job = new JobTask
-            {
-                DeltaTime = Time.deltaTime
-            };
+        //protected override JobHandle OnUpdate(JobHandle inputDeps)
+        //{
+        //    var job = new JobTask
+        //    {
+        //        DeltaTime = Time.deltaTime
+        //    };
 
-            return job.Schedule(this, 64, inputDeps);
-        }
-    }
+        //    return job.Schedule(this, 64, inputDeps);
+        //}
+    //}
 }

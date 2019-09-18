057ccc46a3d78e6f3ac25950e01ee457b5af7bce
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-18 10:39:57 +0900

    20190918
    ECS起動確認。
    update to Unity2019ver2.5f1
    PlayerSettingsの Script Define Symbolの設定を "UNITY_DISABLE_AUTOMATIC_SYSTEM_BOOTSTRAP"とするとワールドが生成されない。
    →切りました。

---
+++ ECS/Assets/Scripts/HelloCube/RotationSpeedProxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Samples.HelloCube
+{
+    [RequiresEntityConversion]
+    public class RotationSpeedProxy : MonoBehaviour,IConvertGameObjectToEntity
+    {
+        public float degreesPerSecond = 360;
+
+        public void Convert(Entity entity,EntityManager dstManager,GameObjectConversionSystem convSystem)
+        {
+            var data = new RotationSpeed
+            {
+                radiansPerSpeed = math.radians(degreesPerSecond)
+            };
+            dstManager.AddComponentData(entity, data);
+        }
+    }
+}
---
+++ ECS/Assets/Scripts/HelloCube/RotationSpeedSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Samples.HelloCube
+{
+
+    public class RotationSpeedSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((ref RotationSpeed rotSpeed, ref Rotation rot) =>
+            {
+                var deltaTime = Time.deltaTime;
+                rot.Value = math.mul(math.normalize(rot.Value),
+                    quaternion.AxisAngle(math.up(), rotSpeed.radiansPerSpeed * deltaTime));
+            });
+        }
+    }
+}

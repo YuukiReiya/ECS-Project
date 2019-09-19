bc55a80ccbceb04bcdfd08ff8ff9085dc3729964
Yuuki Bamba <yuukiBamba@gmail.com>
2019-09-19 09:48:23 +0900

    ECS sample 終了

---
+++ ECS/Assets/Scripts/MySamples/ECS/HybridSpawner/Spawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+namespace Samples.ECS.N
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField] GameObject prefab;
+        [SerializeField] uint spawnPerFrameCount = 100;
+        uint spawnCount;
+        [SerializeField] TMPro.TextMeshProUGUI text;
+        [Header("spawn")]
+        [SerializeField] float intervalTime = 1.0f;
+        float prevTime;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Time.time > prevTime + intervalTime)
+            {
+                Spawn();
+                prevTime = Time.time;
+            }
+            text.text = "SpawnCount:" + spawnCount;
+
+        }
+
+        void Spawn()
+        {
+            Entity prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(this.prefab, World.Active);
+            var entityManager = World.Active.EntityManager;
+            for(int i=0;i<spawnPerFrameCount;++i)
+            {
+                var inst = entityManager.Instantiate(prefab);
+                
+                //生成位置
+                const float heigh = 5.71f;//y
+                const float r = 100;
+                var pos = transform.TransformPoint(new float3(UnityEngine.Random.Range(-r, r), heigh,UnityEngine.Random.Range(-r, r)));
+                entityManager.SetComponentData(inst, new Translation() { Value = pos });
+                spawnCount++;
+            }
+        }
+    }
+}
